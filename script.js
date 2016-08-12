d2c0889813601e671bb8b288afc119d3b7357705
Trev <trevor.pawlewicz@gmail.com>
2016-08-12 18:16:04 -0400

    added ball movement

--- script.js
+++ script.js
@@ -3,20 +3,34 @@ var canvas;
 var canvasContext;
 var ballX = 50;
 var ballY = 0;
+var ballSpeedX = 5;
 
 window.onload = function(){
     console.log("window loaded -- javascript!");
 
     canvas = document.getElementById('gameCanvas'); // index.html
     canvasContext = canvas.getContext('2d');
-    drawEverything();
+
+    var framesPerSecond = 30;
+    setInterval(function(){
+        drawEverything();
+        moveEverything();
+    }, 1000/framesPerSecond);
 }
 
 
 function drawEverything(){
+    // shape order determines the Z axis (first is background, last is front)
     canvasContext.fillStyle = 'black';
     //                     x, y, width,        height
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(0, 210, 10, 100);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(ballX, 100, 10, 10);
+}
 
-    // shape order determines the Z axis
+function moveEverything() {
+    ballX = ballX + 5;
+    ballSpeedX = ballSpeedX + 1;
 }
